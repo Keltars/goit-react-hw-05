1f078d5c7b5f653605a8b5e345d1b2eef80f64fe
Keltars <keltars2000@gmail.com>
2024-07-16 15:16:22 +0300

    components + pages created , home + not found done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+
+import Navigation from "../Navigation/Navigation";
+import HomePage from "../../pages/HomePage";
+import MoviesPage from "../../pages/MoviesPage";
+import NotFoundPage from "../../pages/NotFoundPage";
+
 import css from "./App.module.css";
 
 export default function App() {
-  return <></>;
+  return (
+    <div>
+      <Navigation />
+      <Routes>
+        <Route path={"/"} element={<HomePage />} />
+        <Route path={"/movies"} element={<MoviesPage />} />
+
+        <Route path={"*"} element={<NotFoundPage />} />
+      </Routes>
+    </div>
+  );
 }
---
+++ src/helpers/movies-api.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export const getTrendingMovies = async () => {
+  const res = await axios.get(
+    "https://api.themoviedb.org/3/trending/movie/day?language=ISO-639-1'",
+    {
+      headers: {
+        Accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYmI4NWNjMWVhZGRmOTQ3Y2IyOTFkZDg2ZTdhNDI0MyIsIm5iZiI6MTcyMTEyNzA5MC4zMjM5MzQsInN1YiI6IjY2OGUzODljZDZhN2VmZmY2MzVmNTZlMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.D1Y0PEFUb9lQcmSeR_nDhmRHp5LiPdmTnlbcONLiJEs",
+      },
+    }
+  );
+  return res.data;
+};
